a2ec52f8377af86b398bd97bebef75554745743d
Paul McGrath <paul@sciphus.com>
2017-07-22 14:01:13 +0100

    Add solution for up numbers up to 100

--- NumbersToWords.Tests/WordifierTests.cs
+++ NumbersToWords.Tests/WordifierTests.cs
@@ -5,15 +5,53 @@ namespace NumbersToWords.Tests
     [TestFixture]
     public class WordifierTests
     {
-        [Test]
-        public void DoTheThings()
+        [TestCase(1, "One")]
+        [TestCase(2, "Two")]
+        [TestCase(3, "Three")]
+        [TestCase(4, "Four")]
+        [TestCase(5, "Five")]
+        [TestCase(6, "Six")]
+        [TestCase(7, "Seven")]
+        [TestCase(8, "Eight")]
+        [TestCase(9, "Nine")]
+        public void Convert_WordsForDigits(int input, string expected)
         {
             var unit = new Wordifier();
 
-            var result = unit.Convert(1);
+            var result = unit.Convert(input);
 
-            Assert.That(result, Is.EqualTo("One"));
+            Assert.That(result, Is.EqualTo(expected));
         }
-        
+
+        [TestCase(10, "Ten")]
+        [TestCase(20, "Twenty")]
+        [TestCase(30, "Thirty")]
+        [TestCase(40, "Fourty")]
+        [TestCase(50, "Fifty")]
+        [TestCase(60, "Sixty")]
+        [TestCase(70, "Seventy")]
+        [TestCase(80, "Eighty")]
+        [TestCase(90, "Ninety")]
+        public void Convert_WordsForDecades(int input, string expected)
+        {
+            var unit = new Wordifier();
+
+            var result = unit.Convert(input);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(53, "Fifty-Three")]
+        [TestCase(99, "Ninety-Nine")]
+        [TestCase(17, "Seventeen")]
+        [TestCase(13, "Thirteen")]
+        public void Convert_WordsForNumbersLessThan100(int input, string expected)
+        {
+            var unit = new Wordifier();
+
+            var result = unit.Convert(input);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
     }
 }
--- NumbersToWords/Wordifier.cs
+++ NumbersToWords/Wordifier.cs
@@ -1,10 +1,50 @@
-﻿namespace NumbersToWords
+﻿using System.Collections.Generic;
+
+namespace NumbersToWords
 {
     public class Wordifier
     {
+        Dictionary<int, string> words = new Dictionary<int, string>
+        {
+                    {1,"One"},
+                    {2,"Two"},
+                    {3,"Three"},
+                    {4,"Four"},
+                    {5,"Five"},
+                    {6,"Six"},
+                    {7,"Seven"},
+                    {8,"Eight"},
+                    {9,"Nine"},
+                    {10,"Ten"},
+                    {11,"Eleven"},
+                    {12,"Twelve"},
+                    {13,"Thirteen"},
+                    {14,"Fourteen"},
+                    {15,"Fifteen"},
+                    {16,"Sixteen"},
+                    {17,"Seventeen"},
+                    {18,"Eighteen"},
+                    {19,"Nineteen"},
+                    {20,"Twenty"},
+                    {30,"Thirty"},
+                    {40,"Fourty"},
+                    {50,"Fifty"},
+                    {60,"Sixty"},
+                    {70,"Seventy"},
+                    {80,"Eighty"},
+                    {90,"Ninety"},
+        }; 
+
         public string Convert(int number)
         {
-            return "One";
+            if (words.ContainsKey(number))
+            {
+                return words[number];
+            }
+            
+            int decade = (number/10)*10;
+            var remainder = number%10;
+            return $"{words[decade]}-{words[remainder]}";
         }
     }
 }
